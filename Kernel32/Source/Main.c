c6873107979eb1871ac0b3cc768feb7d91a94f30
UNKNOWN <unknown.zero.un@gmail.com>
2018-11-05 17:42:08 +0900

    Add C main kernel entry

---
+++ Kernel32/Source/Main.c
@@ -0,0 +1,40 @@
+/* filename          /Kernel32/src/Main.c
+ * date              2018.11.05
+ * last edit date    2018.11.05
+ * author            NO.00[UNKNOWN]
+ * brief             start point for C kernel
+*/
+#include "Types.h"
+
+void kPrintString( int iX, int iY, const char* pcString );
+
+/**
+ *  Start Point for C Kernel
+ *  MUST BE FIRST PLACE !!!
+ */
+void Main( void )
+{
+    kPrintString( 0, 3, "C Language Kernel Started~!!!" );
+
+    while( 1 ) ;
+}
+
+/**
+ *  function name : kPrintString
+ *  Parameters    : iX(int) - screen x corr
+ *                  iY(int) - screen y corr
+ *                  pcStirng(const char*) - string address
+ *  brief         : print string to screen
+ */
+void kPrintString( int iX, int iY, const char* pcString )
+{
+    CHARACTER* pstScreen = ( CHARACTER* ) 0xB8000;
+    int i;
+    
+    pstScreen += ( iY * 80 ) + iX;
+    
+    for ( i = 0 ; pcString[ i ] != 0 ; i++ )
+    {
+        pstScreen[ i ].bCharactor = pcString[ i ];
+    }
+}
---
+++ Kernel32/Source/Types.h
@@ -0,0 +1,34 @@
+/* filename          /Kernel32/src/Types.h
+ * date              2018.11.05
+ * last edit date    2018.11.05
+ * author            NO.00[UNKNOWN]
+ * brief             define entry point for kernel32 c function
+*/
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#define BYTE    unsigned char
+#define WORD    unsigned short
+#define DWORD   unsigned int
+#define QWORD   unsigned long
+#define BOOL    unsigned char
+
+#define TRUE    1
+#define FALSE   0
+#define NULL    0
+
+
+//      STRUCT      //
+#pragma pack( push, 1 )
+
+// data for text video screen
+typedef struct kCharactorStruct
+{
+    BYTE bCharactor;
+    BYTE bAttribute;
+} CHARACTER;
+
+#pragma pack( pop )
+
+#endif /*__TYPES_H__*/
