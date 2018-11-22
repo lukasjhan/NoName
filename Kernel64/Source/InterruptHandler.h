23c0ba6671af8ff9b681a563f38a772656ce25e4
UNKNOWN <unknown.zero.un@gmail.com>
2018-11-22 16:42:42 +0900

    Edit date

--- Kernel64/Source/InterruptHandler.h
+++ Kernel64/Source/InterruptHandler.h
@@ -1,6 +1,6 @@
 /* filename          /Kernel64/Source/InterruptHandler.h
- * date              2018.11.20
- * last edit date    2018.11.20
+ * date              2018.11.22
+ * last edit date    2018.11.22
  * author            NO.00[UNKNOWN]
  * brief             header file for InterruptHandler.c
 */
