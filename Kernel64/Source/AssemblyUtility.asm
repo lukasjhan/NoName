8f678b92663d583de213672fa51a96cb445bad20
UNKNOWN <unknown.zero.un@gmail.com>
2018-11-27 10:48:32 +0900

    Add timestamp function

--- Kernel64/Source/AssemblyUtility.asm
+++ Kernel64/Source/AssemblyUtility.asm
@@ -10,6 +10,7 @@ SECTION .text
 
 global kInPortByte, kOutPortByte, kLoadGDTR, kLoadTR, kLoadIDTR
 global kEnableInterrupt, kDisableInterrupt, kReadRFLAGS
+global kReadTSC
 
 ; function name : kInPortByte
 ; parametor     : port number
@@ -81,4 +82,18 @@ kDisableInterrupt:
 kReadRFLAGS:
     pushfq                  ; save RFLAGS register into stack
     pop rax                 ; save RFLAGS register int RAX register to set return value
+    ret
+
+; function name : kReadTSC
+; parametor     : NONE
+; brief         : Return Timestamp counter
+kReadTSC:
+    push rdx
+    
+    rdtsc                   ; Read Timestamp data and store it into RDX:RAX
+    
+    shl rdx, 32
+    or rax, rdx
+    
+    pop rdx
     ret
