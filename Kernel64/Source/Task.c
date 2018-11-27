9d00c101228c4992203cd7836ad426f940359191
UNKNOWN <unknown.zero.un@gmail.com>
2018-11-27 17:02:13 +0900

    Edit task

--- Kernel64/Source/Task.c
+++ Kernel64/Source/Task.c
@@ -8,6 +8,108 @@
 #include "Task.h"
 #include "Descriptor.h"
 
+static SCHEDULER gs_stScheduler;
+static TCBPOOLMANAGER gs_stTCBPoolManager;
+
+//  TASK
+
+/**
+ *  function name : kInitializeTCBPool
+ *  parameters    : void
+ *  return value  : void
+ *  brief         : init taskpool
+ */
+void kInitializeTCBPool( void )
+{
+    int i;
+    
+    kMemSet( &( gs_stTCBPoolManager ), 0, sizeof( gs_stTCBPoolManager ) );
+    
+    gs_stTCBPoolManager.pstStartAddress = ( TCB* ) TASK_TCBPOOLADDRESS;
+    kMemSet( TASK_TCBPOOLADDRESS, 0, sizeof( TCB ) * TASK_MAXCOUNT );
+
+    for ( i = 0 ; i < TASK_MAXCOUNT ; i++ )
+        gs_stTCBPoolManager.pstStartAddress[ i ].stLink.qwID = i;
+    
+    gs_stTCBPoolManager.iMaxCount = TASK_MAXCOUNT;
+    gs_stTCBPoolManager.iAllocatedCount = 1;
+}
+
+/**
+ *  function name : kAllocateTCB
+ *  parameters    : void
+ *  return value  : TCB*
+ *  brief         : allocate TCB
+ */
+TCB* kAllocateTCB( void )
+{
+    TCB*    pstEmptyTCB;
+    int     i;
+    
+    if ( gs_stTCBPoolManager.iUseCount == gs_stTCBPoolManager.iMaxCount )
+        return NULL;
+    
+    for ( i = 0 ; i < gs_stTCBPoolManager.iMaxCount ; i++ )
+    {
+        if ( ( gs_stTCBPoolManager.pstStartAddress[ i ].stLink.qwID >> 32 ) == 0 )
+        {
+            pstEmptyTCB = &( gs_stTCBPoolManager.pstStartAddress[ i ] );
+            break;
+        }
+    }
+
+    pstEmptyTCB->stLink.qwID = ( ( QWORD ) gs_stTCBPoolManager.iAllocatedCount << 32 ) | i;
+    gs_stTCBPoolManager.iUseCount++;
+    gs_stTCBPoolManager.iAllocatedCount++;
+
+    if ( gs_stTCBPoolManager.iAllocatedCount == 0 )
+        gs_stTCBPoolManager.iAllocatedCount = 1;
+    
+    return pstEmptyTCB;
+}
+
+/**
+ *  function name : kFreeTCB
+ *  parameters    : qwID(QWORD)
+ *  return value  : void
+ *  brief         : deallocate TCB
+ */
+void kFreeTCB( QWORD qwID )
+{
+    int i;
+    
+    i = qwID & 0xFFFFFFFF;
+    
+    kMemSet( &( gs_stTCBPoolManager.pstStartAddress[ i ].stContext ), 0, sizeof( CONTEXT ) );
+    gs_stTCBPoolManager.pstStartAddress[ i ].stLink.qwID = i;
+    
+    gs_stTCBPoolManager.iUseCount--;
+}
+
+/**
+ *  function name : kCreateTask
+ *  parameters    : qwFlags(QWORD)
+ *                  qwEntryPointAddress(QWORD)
+ *  return value  : TCB*
+ *  brief         : create task
+ */
+TCB* kCreateTask( QWORD qwFlags, QWORD qwEntryPointAddress )
+{
+    TCB*    pstTask;
+    void*   pvStackAddress;
+    
+    pstTask = kAllocateTCB();
+    if ( pstTask == NULL )
+        return NULL;
+    
+    pvStackAddress = ( void* ) ( TASK_STACKPOOLADDRESS + ( TASK_STACKSIZE * ( pstTask->stLink.qwID & 0xFFFFFFFF ) ) );
+    
+    kSetUpTask( pstTask, qwFlags, qwEntryPointAddress, pvStackAddress, TASK_STACKSIZE );
+    kAddTaskToReadyList( pstTask );
+    
+    return pstTask;
+}
+
 /**
  *  function name : kSetUpTask
  *  parameters    : pstTCB(TCB*)
@@ -19,31 +121,182 @@
  *  return value  : void
  *  brief         : set up TCB
  */
-void kSetUpTask( TCB* pstTCB, QWORD qwID, QWORD qwFlags, QWORD qwEntryPointAddress, void* pvStackAddress, QWORD qwStackSize )
+void kSetUpTask( TCB* pstTCB, QWORD qwFlags, QWORD qwEntryPointAddress, void* pvStackAddress, QWORD qwStackSize )
 {
-    // init context
     kMemSet( pstTCB->stContext.vqRegister, 0, sizeof( pstTCB->stContext.vqRegister ) );
     
-    // set up stack RSP, RBP register
     pstTCB->stContext.vqRegister[ TASK_RSPOFFSET ] = ( QWORD ) pvStackAddress + qwStackSize;
     pstTCB->stContext.vqRegister[ TASK_RBPOFFSET ] = ( QWORD ) pvStackAddress + qwStackSize;
 
-    // set up segment selector
     pstTCB->stContext.vqRegister[ TASK_CSOFFSET ] = GDT_KERNELCODESEGMENT;
     pstTCB->stContext.vqRegister[ TASK_DSOFFSET ] = GDT_KERNELDATASEGMENT;
     pstTCB->stContext.vqRegister[ TASK_ESOFFSET ] = GDT_KERNELDATASEGMENT;
     pstTCB->stContext.vqRegister[ TASK_FSOFFSET ] = GDT_KERNELDATASEGMENT;
     pstTCB->stContext.vqRegister[ TASK_GSOFFSET ] = GDT_KERNELDATASEGMENT;
     pstTCB->stContext.vqRegister[ TASK_SSOFFSET ] = GDT_KERNELDATASEGMENT;
-
-    // set up RIP register, interrupt flag 
+    
     pstTCB->stContext.vqRegister[ TASK_RIPOFFSET ] = qwEntryPointAddress;
 
-    // RFLAGS register IF bit(bit 9) set 1 to enable interrupt
     pstTCB->stContext.vqRegister[ TASK_RFLAGSOFFSET ] |= 0x0200;
     
-    pstTCB->qwID            = qwID;
     pstTCB->pvStackAddress  = pvStackAddress;
     pstTCB->qwStackSize     = qwStackSize;
     pstTCB->qwFlags         = qwFlags;
+}
+
+//  Scheduler
+
+/**
+ *  function name : kInitializeScheduler
+ *  parameters    : void
+ *  return value  : void
+ *  brief         : init scheduler
+ */
+void kInitializeScheduler( void )
+{
+    kInitializeTCBPool();
+
+    kInitializeList( &( gs_stScheduler.stReadyList ) );
+
+    gs_stScheduler.pstRunningTask = kAllocateTCB();
+}
+
+/**
+ *  function name : kSetRunningTask
+ *  parameters    : pstTask(TCB*)
+ *  return value  : void
+ *  brief         : set running task
+ */
+void kSetRunningTask( TCB* pstTask )
+{
+    gs_stScheduler.pstRunningTask = pstTask;
+}
+
+/**
+ *  function name : kGetRunningTask
+ *  parameters    : void
+ *  return value  : TCB*
+ *  brief         : return running task
+ */
+TCB* kGetRunningTask( void )
+{
+    return gs_stScheduler.pstRunningTask;
+}
+
+/**
+ *  function name : kGetNextTaskToRun
+ *  parameters    : void
+ *  return value  : TCB*
+ *  brief         : return next running task
+ */
+TCB* kGetNextTaskToRun( void )
+{
+    if ( kGetListCount( &( gs_stScheduler.stReadyList ) ) == 0 )
+        return NULL;
+    
+    return ( TCB* ) kRemoveListFromHeader( &( gs_stScheduler.stReadyList ) );
+}
+
+/**
+ *  function name : kAddTaskToReadyList
+ *  parameters    : pstTask(TCB*)
+ *  return value  : void
+ *  brief         : add task into list
+ */
+void kAddTaskToReadyList( TCB* pstTask )
+{
+    kAddListToTail( &( gs_stScheduler.stReadyList ), pstTask );
+}
+
+/**
+ *  function name : kSchedule
+ *  parameters    : void
+ *  return value  : void
+ *  brief         : schedule function
+ *                  DO NOT CALL IT DURING INTERRUPT OR EXCEPTION!!!
+ */
+void kSchedule( void )
+{
+    TCB* pstRunningTask, * pstNextTask;
+    BOOL bPreviousFlag;
+    
+    if ( kGetListCount( &( gs_stScheduler.stReadyList ) ) == 0 )
+        return ;
+    
+    // disable interrupt
+    bPreviousFlag = kSetInterruptFlag( FALSE );
+    pstNextTask = kGetNextTaskToRun();
+
+    if ( pstNextTask == NULL )
+    {
+        kSetInterruptFlag( bPreviousFlag );
+        return ;
+    }
+    
+    pstRunningTask = gs_stScheduler.pstRunningTask; 
+    kAddTaskToReadyList( pstRunningTask );
+
+    gs_stScheduler.iProcessorTime = TASK_PROCESSORTIME;
+    
+    gs_stScheduler.pstRunningTask = pstNextTask;
+    kSwitchContext( &( pstRunningTask->stContext ), &( pstNextTask->stContext ) );
+
+    kSetInterruptFlag( bPreviousFlag );
+}
+
+/**
+ *  function name : kScheduleInInterrupt
+ *  parameters    : void
+ *  return value  : BOOL
+ *  brief         : when interrupt occur, switching task
+ *                  CALL IT DURING INTERRUPT OR EXCEPTION!!!
+ */
+BOOL kScheduleInInterrupt( void )
+{
+    TCB* pstRunningTask, * pstNextTask;
+    char* pcContextAddress;
+    
+    pstNextTask = kGetNextTaskToRun();
+
+    if ( pstNextTask == NULL )
+        return FALSE;
+    
+    pcContextAddress = ( char* ) IST_STARTADDRESS + IST_SIZE - sizeof( CONTEXT );
+    
+    pstRunningTask = gs_stScheduler.pstRunningTask;
+    kMemCpy( &( pstRunningTask->stContext ), pcContextAddress, sizeof( CONTEXT ) );
+    kAddTaskToReadyList( pstRunningTask );
+
+    gs_stScheduler.pstRunningTask = pstNextTask;
+    kMemCpy( pcContextAddress, &( pstNextTask->stContext ), sizeof( CONTEXT ) );
+    
+    gs_stScheduler.iProcessorTime = TASK_PROCESSORTIME;
+    return TRUE;
+}
+
+/**
+ *  function name : kDecreaseProcessorTime
+ *  parameters    : void
+ *  return value  : void
+ *  brief         : decrease processor time
+ */
+void kDecreaseProcessorTime( void )
+{
+    if ( gs_stScheduler.iProcessorTime > 0 )
+        gs_stScheduler.iProcessorTime--;
+    
+}
+
+/**
+ *  function name : kIsProcessorTimeExpired
+ *  parameters    : void
+ *  return value  : void
+ *  brief         : return processor time expired
+ */
+BOOL kIsProcessorTimeExpired( void )
+{
+    if ( gs_stScheduler.iProcessorTime <= 0 )
+        return TRUE;
+    
+    return FALSE;
 }
--- Kernel64/Source/Task.h
+++ Kernel64/Source/Task.h
@@ -9,6 +9,7 @@
 #define __TASK_H__
 
 #include "Types.h"
+#include "List.h"
 
 #define TASK_REGISTERCOUNT     ( 5 + 19 )
 #define TASK_REGISTERSIZE       8
@@ -38,30 +39,72 @@
 #define TASK_RSPOFFSET          22
 #define TASK_SSOFFSET           23
 
-// align 1 byte
+#define TASK_TCBPOOLADDRESS     0x800000
+#define TASK_MAXCOUNT           1024
+
+#define TASK_STACKPOOLADDRESS   ( TASK_TCBPOOLADDRESS + sizeof( TCB ) * TASK_MAXCOUNT )
+#define TASK_STACKSIZE          8192
+
+#define TASK_INVALIDID          0xFFFFFFFFFFFFFFFF
+
+#define TASK_PROCESSORTIME      5
+
+
 #pragma pack( push, 1 )
 
-// context
 typedef struct kContextStruct
 {
     QWORD vqRegister[ TASK_REGISTERCOUNT ];
 } CONTEXT;
 
-// task info
 typedef struct kTaskControlBlockStruct
 {
-    CONTEXT stContext;
-
-    // ID, FLAG
-    QWORD qwID;
+    LISTLINK stLink;
+    
     QWORD qwFlags;
+
+    CONTEXT stContext;
 
     void* pvStackAddress;
     QWORD qwStackSize;
 } TCB;
 
+typedef struct kTCBPoolManagerStruct
+{
+    TCB* pstStartAddress;
+    int iMaxCount;
+    int iUseCount;
+    
+    int iAllocatedCount;
+} TCBPOOLMANAGER;
+
+typedef struct kSchedulerStruct
+{
+    TCB* pstRunningTask;
+    
+    int iProcessorTime;
+    
+    LIST stReadyList;
+} SCHEDULER;
+
 #pragma pack( pop )
 
-void kSetUpTask( TCB* pstTCB, QWORD qwID, QWORD qwFlags, QWORD qwEntryPointAddress, void* pvStackAddress, QWORD qwStackSize );
+// TASK
+void kInitializeTCBPool( void );
+TCB* kAllocateTCB( void );
+void kFreeTCB( QWORD qwID );
+TCB* kCreateTask( QWORD qwFlags, QWORD qwEntryPointAddress );
+void kSetUpTask( TCB* pstTCB, QWORD qwFlags, QWORD qwEntryPointAddress, void* pvStackAddress, QWORD qwStackSize );
+
+// Scheduler
+void kInitializeScheduler( void );
+void kSetRunningTask( TCB* pstTask );
+TCB* kGetRunningTask( void );
+TCB* kGetNextTaskToRun( void );
+void kAddTaskToReadyList( TCB* pstTask );
+void kSchedule( void );
+BOOL kScheduleInInterrupt( void );
+void kDecreaseProcessorTime( void );
+BOOL kIsProcessorTimeExpired( void );
 
 #endif /*__TASK_H__*/
