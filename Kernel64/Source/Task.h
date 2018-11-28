feb905ffc1e2cb70688ca661c146db9362c52a5d
UNKNOWN <unknown.zero.un@gmail.com>
2018-11-28 18:32:59 +0900

    Edit task functions with mutex

--- Kernel64/Source/Task.h
+++ Kernel64/Source/Task.h
@@ -115,23 +115,23 @@ typedef struct kSchedulerStruct
 #pragma pack( pop )
 
 // TASK
-void kInitializeTCBPool( void );
-TCB* kAllocateTCB( void );
-void kFreeTCB( QWORD qwID );
+static void kInitializeTCBPool( void );
+static TCB* kAllocateTCB( void );
+static void kFreeTCB( QWORD qwID );
 TCB* kCreateTask( QWORD qwFlags, QWORD qwEntryPointAddress );
-void kSetUpTask( TCB* pstTCB, QWORD qwFlags, QWORD qwEntryPointAddress, void* pvStackAddress, QWORD qwStackSize );
+static void kSetUpTask( TCB* pstTCB, QWORD qwFlags, QWORD qwEntryPointAddress, void* pvStackAddress, QWORD qwStackSize );
 
 // Scheduler
 void kInitializeScheduler( void );
 void kSetRunningTask( TCB* pstTask );
 TCB* kGetRunningTask( void );
-TCB* kGetNextTaskToRun( void );
-BOOL kAddTaskToReadyList( TCB* pstTask );
+static TCB* kGetNextTaskToRun( void );
+static BOOL kAddTaskToReadyList( TCB* pstTask );
 void kSchedule( void );
 BOOL kScheduleInInterrupt( void );
 void kDecreaseProcessorTime( void );
 BOOL kIsProcessorTimeExpired( void );
-TCB* kRemoveTaskFromReadyList( QWORD qwTaskID );
+static TCB* kRemoveTaskFromReadyList( QWORD qwTaskID );
 BOOL kChangePriority( QWORD qwID, BYTE bPriority );
 BOOL kEndTask( QWORD qwTaskID );
 void kExitTask( void );
