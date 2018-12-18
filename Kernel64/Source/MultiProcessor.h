bd995ed7c2a99c58958b7b97857cfebe8bdf63c4
UNKNOWN <unknown.zero.un@gmail.com>
2018-12-18 14:19:54 +0900

    Add multi process

---
+++ Kernel64/Source/MultiProcessor.h
@@ -0,0 +1,6 @@
+/* filename          /Kernel64/Source/MultiProcessor.h
+ * date              2018.12.18
+ * last edit date    2018.12.18
+ * author            NO.00[UNKNOWN]
+ * brief             header file for MultiProcessor.c
+*/
